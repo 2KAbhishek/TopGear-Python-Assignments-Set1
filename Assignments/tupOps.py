6b09e61be6e8786a664db056edb59735848002b1
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-04-04 15:40:43 +0530

    Add tupOps.py

---
+++ Assignments/tupOps.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+tup = (4, 5, 7, 1, 2, 9, 6, 3, 8, 0)
+print('Tuple elements:', tup)
+print('Sliced tuple:', tup[: -1: 2])
+print('Repeated tuple:', tup * 2)
+
+tup2 = (12, 11, 14, 13)
+print('Concatenated tuple:', tup + tup2)

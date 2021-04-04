9acbd7ce6f80cc85b477ca93815c643bac7400a1
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-04-04 15:40:41 +0530

    Add assignOps.py

---
+++ Assignments/assignOps.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+a = int(input('Enter first no: '))
+b = int(input('Enter second no: '))
+a1 = a2 = a3 = a4 = a5 = a6 = a
+
+a1 += b
+print(f'Sum is {a1}')
+
+a2 -= b
+print(f'Subtraction is {a2}')
+
+a3 *= b
+print(f'Multiplication is {a3}')
+
+a4 /= b
+print(f'Division is {a4}')
+
+a5 **= b
+print(f'{a} to the Power {b} is {a5}')
+
+a6 //= b
+print(f'Floor Division is {a6}')

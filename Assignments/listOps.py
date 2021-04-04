942caead0463339949dd603738fc906abc981ef6
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-04-04 15:40:42 +0530

    Add listOps.py

---
+++ Assignments/listOps.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+lst = [4, 5, 7, 1, 2, 9, 6, 3, 8, 0]
+print('List elements:', lst)
+print('Sliced list:', lst[1: 10: 2])
+print('Repeated list:', lst * 2)
+lst2 = [12, 11, 14, 13]
+print('Concatenated list:', lst + lst2)
